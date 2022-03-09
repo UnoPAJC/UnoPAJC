2c28037147e309dd3e45f45aec704de2aa6cf36e
Stefano Valloncini <stefanovalloncini@protonmail.com>
2022-03-09 14:18:15 +0100

    Update

--- src/unibs/pajc/uno/view/CardView.java
+++ src/unibs/pajc/uno/view/CardView.java
@@ -1,134 +1,156 @@
 package unibs.pajc.uno.view;
 
-import domain.card.Card;
-import ui.common.StyleUtil;
-
-import javax.swing.*;
-import javax.swing.border.Border;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.function.Consumer;
 
-public class CardView extends JPanel {
-    private final Card card;
-    private final String value;
+// import ui.common.StyleUtil;
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
 
-    private final int cardWidth = 100;
-    private final int cardHeight = 150;
-    private static final int margin = 5;
+public class CardView extends JPanel
+{
+	private final Card card;
+	private final String value;
 
-    private final Dimension dimension = new Dimension(cardWidth, cardHeight);
+	private final int cardWidth = 100;
+	private final int cardHeight = 150;
+	private static final int margin = 5;
 
-    private final Border defaultBorder = BorderFactory.createEtchedBorder(WHEN_FOCUSED, Color.white, Color.gray);
-    private final Border focusedBorder = BorderFactory.createEtchedBorder(WHEN_FOCUSED, Color.black, Color.gray);
+	private final Dimension dimension = new Dimension(cardWidth, cardHeight);
 
-    private final Consumer<Card> handleCardClick;
+	private final Border defaultBorder = BorderFactory.createEtchedBorder(WHEN_FOCUSED, Color.white, Color.gray);
+	private final Border focusedBorder = BorderFactory.createEtchedBorder(WHEN_FOCUSED, Color.black, Color.gray);
 
-    public CardView(Card card) {
-        this(card, null);
-    }
+	private final Consumer<Card> handleCardClick;
 
-    public CardView(Card card, Consumer<Card> onCardClick){
-        this.card = card;
-        this.handleCardClick = onCardClick;
-        this.value = StyleUtil.getValueToDisplay(card);
+	public CardView(Card card)
+	{
+		this(card, null);
+	}
 
-        initView();
-    }
+	public CardView(Card card, Consumer<Card> onCardClick)
+	{
+		this.card = card;
+		this.handleCardClick = onCardClick;
+		this.value = StyleUtil.getValueToDisplay(card);
 
-    public Dimension getDimension() {
-        return dimension;
-    }
+		initView();
+	}
 
-    private void initView() {
-        setPreferredSize(dimension);
-        setBorder(defaultBorder);
+	public Dimension getDimension()
+	{
+		return dimension;
+	}
 
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                super.mouseEntered(e);
-                showHoverEffect();
-            }
+	private void initView()
+	{
+		setPreferredSize(dimension);
+		setBorder(defaultBorder);
 
-            @Override
-            public void mouseExited(MouseEvent e) {
-               super.mouseExited(e);
-               removeHoverEffect();
-            }
+		addMouseListener(new MouseAdapter()
+		{
+			@Override
+			public void mouseEntered(MouseEvent e)
+			{
+				super.mouseEntered(e);
+				showHoverEffect();
+			}
 
-            @Override
-            public void mousePressed(MouseEvent e) {
-                if(handleCardClick != null) {
-                    handleCardClick.accept(card);
-                }
-            }
-        });
-    }
+			@Override
+			public void mouseExited(MouseEvent e)
+			{
+				super.mouseExited(e);
+				removeHoverEffect();
+			}
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+			@Override
+			public void mousePressed(MouseEvent e)
+			{
+				if (handleCardClick != null)
+				{
+					handleCardClick.accept(card);
+				}
+			}
+		});
+	}
 
-        Graphics2D g2 = (Graphics2D) g;
-        var cardColor = StyleUtil.convertCardColor(card.getColor());
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
 
-        fillBackground(g2, cardColor);
-        drawWhiteOvalInCenter(g2);
-        drawValueInCenter(g2, cardColor);
-        drawValueInCorner(g2);
-    }
+		Graphics2D g2 = (Graphics2D) g;
+		var cardColor = StyleUtil.convertCardColor(card.getColor());
 
-    private void fillBackground(Graphics2D g2, Color cardColor) {
-        g2.setColor(Color.WHITE);
-        g2.fillRect(0, 0, cardWidth, cardHeight);
+		fillBackground(g2, cardColor);
+		drawWhiteOvalInCenter(g2);
+		drawValueInCenter(g2, cardColor);
+		drawValueInCorner(g2);
+	}
 
-        g2.setColor(cardColor);
-        g2.fillRect(margin, margin, cardWidth - 2 * margin, cardHeight - 2 * margin);
-    }
+	private void fillBackground(Graphics2D g2, Color cardColor)
+	{
+		g2.setColor(Color.WHITE);
+		g2.fillRect(0, 0, cardWidth, cardHeight);
 
-    private void drawWhiteOvalInCenter(Graphics2D g2) {
-        var transformer = g2.getTransform();
-        g2.setColor(Color.white);
-        g2.rotate(45, (double) cardWidth * 3 / 4, (double) cardHeight * 3 / 4);
-        g2.fillOval(0, cardHeight / 4, cardWidth * 3 / 5, cardHeight);
+		g2.setColor(cardColor);
+		g2.fillRect(margin, margin, cardWidth - 2 * margin, cardHeight - 2 * margin);
+	}
 
-        g2.setTransform(transformer);
-    }
+	private void drawWhiteOvalInCenter(Graphics2D g2)
+	{
+		var transformer = g2.getTransform();
+		g2.setColor(Color.white);
+		g2.rotate(45, (double) cardWidth * 3 / 4, (double) cardHeight * 3 / 4);
+		g2.fillOval(0, cardHeight / 4, cardWidth * 3 / 5, cardHeight);
 
-    private void drawValueInCenter(Graphics2D g2, Color cardColor) {
-        var defaultFont = new Font(StyleUtil.DEFAULT_FONT, Font.BOLD, cardWidth / 2 + 5);
-        var fontMetrics = this.getFontMetrics(defaultFont);
-        int stringWidth = fontMetrics.stringWidth(value) / 2;
-        int fontHeight = defaultFont.getSize() / 3;
+		g2.setTransform(transformer);
+	}
 
-        g2.setColor(cardColor);
-        g2.setFont(defaultFont);
-        g2.drawString(value, cardWidth / 2 - stringWidth, cardHeight / 2 + fontHeight);
-    }
+	private void drawValueInCenter(Graphics2D g2, Color cardColor)
+	{
+		var defaultFont = new Font(StyleUtil.DEFAULT_FONT, Font.BOLD, cardWidth / 2 + 5);
+		var fontMetrics = this.getFontMetrics(defaultFont);
+		int stringWidth = fontMetrics.stringWidth(value) / 2;
+		int fontHeight = defaultFont.getSize() / 3;
 
-    private void drawValueInCorner(Graphics2D g2) {
-        var defaultFont = new Font(StyleUtil.DEFAULT_FONT, Font.ITALIC, cardWidth / 5);
+		g2.setColor(cardColor);
+		g2.setFont(defaultFont);
+		g2.drawString(value, cardWidth / 2 - stringWidth, cardHeight / 2 + fontHeight);
+	}
 
-        g2.setColor(Color.white);
-        g2.setFont(defaultFont);
-        g2.drawString(value, margin, 5 * margin);
-    }
+	private void drawValueInCorner(Graphics2D g2)
+	{
+		var defaultFont = new Font(StyleUtil.DEFAULT_FONT, Font.ITALIC, cardWidth / 5);
 
-    private void showHoverEffect(){
-        setBorder(focusedBorder);
+		g2.setColor(Color.white);
+		g2.setFont(defaultFont);
+		g2.drawString(value, margin, 5 * margin);
+	}
 
-        Point p = getLocation();
-        p.y -= 20;
-        setLocation(p);
-    }
+	private void showHoverEffect()
+	{
+		setBorder(focusedBorder);
 
-    private void removeHoverEffect() {
-        setBorder(defaultBorder);
+		Point p = getLocation();
+		p.y -= 20;
+		setLocation(p);
+	}
 
-        Point p = getLocation();
-        p.y += 20;
-        setLocation(p);
-    }
+	private void removeHoverEffect()
+	{
+		setBorder(defaultBorder);
+
+		Point p = getLocation();
+		p.y += 20;
+		setLocation(p);
+	}
 }
